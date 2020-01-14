327c48c9dae53b626c388c9cdb2b43783e49ea66
Brian Edwards <brianledwards@gmail.com>
2020-01-13 20:14:55 -0500

    Service Interfaces

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PerService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.models.Owner;
+import guru.springframework.sfgpetclinic.models.Pet;
+
+import java.util.Set;
+
+public interface PerService {
+    Pet  findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
