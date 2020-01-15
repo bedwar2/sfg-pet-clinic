5ffbd433470d3b1cedbd48acc7d048c4fb4f6a68
Brian Edwards <brianledwards@gmail.com>
2020-01-14 23:22:18 -0500

    Map Services implementations

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework.sfgpetclinic.services.map;
-
-import guru.springframework.sfgpetclinic.models.Owner;
-
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> {
-
-
-}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PersonMapService.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.models.Person;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PersonMapService extends AbstractMapService<Person, Long> implements CrudService<Person, Long> {
+
+    @Override
+    public Set<Person> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Person person) {
+        super.delete(person);
+    }
+
+    @Override
+    public Person save(Person person) {
+        return super.save(person.getId(), person);
+    }
+
+    @Override
+    public Person findById(Long id) {
+        return super.findById(id);
+    }
+}
