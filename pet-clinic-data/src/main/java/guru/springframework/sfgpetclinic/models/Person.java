dc641885dcf975bf99d9bdc7fc40a65633cd2c2f
Brian Edwards <brianledwards@gmail.com>
2020-01-18 13:44:22 -0500

    Add Visit Entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Person.java
@@ -1,9 +1,12 @@
 package guru.springframework.sfgpetclinic.models;
 
+import java.util.Set;
+
 public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
+    private Set<Pet> pets;
 
     public String getFirstName() {
         return firstName;
@@ -19,5 +22,13 @@ public class Person extends BaseEntity {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
