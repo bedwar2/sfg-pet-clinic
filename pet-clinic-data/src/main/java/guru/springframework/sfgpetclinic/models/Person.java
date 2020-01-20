5dfb2d565a8c89c1d20ee6b028745fd05096fa01
Brian Edwards <brianledwards@gmail.com>
2020-01-20 11:25:59 -0500

    Add JPA Annotations

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Person.java
@@ -1,10 +1,16 @@
 package guru.springframework.sfgpetclinic.models;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
 import java.util.Set;
 
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 
 
